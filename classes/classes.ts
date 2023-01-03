2624a94ccc4002acbdd8d41a5a72557d5c80d0ca
jardel88 <jldv@gft.com>
2023-01-03 19:28:30 -0300

    Atributos somente leitura

--- classes/classes.ts
+++ classes/classes.ts
@@ -187,3 +187,17 @@ class Unico {
 
 // const errado = new Unico()
 console.log(Unico.getInstance().agora())
+
+class Aviao {
+    public readonly modelo: string
+
+    constructor(modelo: string,
+        public readonly prefixo: string) {
+        this.modelo = modelo    
+    }
+}
+
+const turboHelice = new Aviao('Tu-114', 'PT-ABC')
+//turboHelice.modelo = 'DC-8'
+//turboHelice.prefixo = 'PT-DEF'
+console.log(turboHelice)
