63fe57c9f6652e8bbb381babec15d1dc8fece053
jardel88 <jldv@gft.com>
2023-01-03 13:24:18 -0300

    Classes

--- classes/classes.js
+++ classes/classes.js
@@ -10,3 +10,14 @@ const aniversario = new Data(7, 3, 1988);
 aniversario.dia = 4;
 console.log(aniversario.dia);
 console.log(aniversario);
+class DataTeste {
+    constructor(dia = 1, mes = 1, ano = 1970) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+}
+const aniversarioTeste = new DataTeste(7, 3, 1988);
+aniversarioTeste.dia = 4;
+console.log(aniversarioTeste.dia);
+console.log(aniversarioTeste);
--- classes/classes.ts
+++ classes/classes.ts
@@ -15,3 +15,14 @@ const aniversario = new Data(7, 3, 1988)
 aniversario.dia = 4
 console.log(aniversario.dia)
 console.log(aniversario)
+
+class DataTeste {
+
+    constructor(public dia: number = 1, public mes: number = 1, public ano: number = 1970){}
+
+}
+
+const aniversarioTeste = new DataTeste(7, 3, 1988)
+aniversarioTeste.dia = 4
+console.log(aniversarioTeste.dia)
+console.log(aniversarioTeste)
