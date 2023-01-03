9a6b2dd8188d6af16d4d4f72efcd54dd85b2e722
jardel88 <jldv@gft.com>
2023-01-03 17:26:33 -0300

    Modificadores de acesso

--- classes/classes.js
+++ classes/classes.js
@@ -36,3 +36,34 @@ prod1.desconto = 0.05;
 console.log(prod1.resumo());
 const prod2 = new Produto('Caderno escolar', 18.80, 0.15);
 console.log(prod2.resumo());
+class Carro {
+    constructor(marca, modelo, velocidadeMaxima = 200) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.velocidadeAtual = 0;
+    }
+    alterarVelocidade(delta) {
+        const novaVelocidade = this.velocidadeAtual + delta;
+        const velocidadeValida = novaVelocidade >= 0
+            && novaVelocidade <= this.velocidadeMaxima;
+        if (velocidadeValida) {
+            this.velocidadeAtual = novaVelocidade;
+        }
+        else {
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0;
+        }
+        return this.velocidadeAtual;
+    }
+    acelerar() {
+        return this.alterarVelocidade(5);
+    }
+    frear() {
+        return this.alterarVelocidade(-5);
+    }
+}
+const carro1 = new Carro('ford', 'ka', 185);
+Array(50).fill(0).forEach(() => carro1.acelerar());
+console.log(carro1.acelerar());
+Array(20).fill(0).forEach(() => carro1.frear());
+console.log(carro1.acelerar());
