fcdbb8735bdaa26da5524e583f009a2ec61ab376
jardel88 <jardelsilvagomes@hotmail.com>
2023-01-05 08:07:21 -0300

    Namespaces aninhados

--- namespaces/namespaces.js
+++ namespaces/namespaces.js
@@ -1,17 +1,20 @@
 "use strict";
-var Areas;
-(function (Areas) {
-    const PI = 3.14;
-    function areaCircunferencia(raio) {
-        return PI * Math.pow(raio, 2);
-    }
-    Areas.areaCircunferencia = areaCircunferencia;
-    function areaRetangulo(base, altura) {
-        return base * altura;
-    }
-    Areas.areaRetangulo = areaRetangulo;
-})(Areas || (Areas = {}));
+var Geometria;
+(function (Geometria) {
+    let Area;
+    (function (Area) {
+        const PI = 3.14;
+        function areaCircunferencia(raio) {
+            return PI * Math.pow(raio, 2);
+        }
+        Area.areaCircunferencia = areaCircunferencia;
+        function areaRetangulo(base, altura) {
+            return base * altura;
+        }
+        Area.areaRetangulo = areaRetangulo;
+    })(Area = Geometria.Area || (Geometria.Area = {}));
+})(Geometria || (Geometria = {}));
 const PI = 2.99;
-console.log(Areas.areaCircunferencia(10));
-console.log(Areas.areaRetangulo(10, 20));
+console.log(Geometria.Area.areaCircunferencia(10));
+console.log(Geometria.Area.areaRetangulo(10, 20));
 console.log(PI);
--- namespaces/namespaces.ts
+++ namespaces/namespaces.ts
@@ -1,19 +1,21 @@
-namespace Areas {
+namespace Geometria {
+    export namespace Area {
 
-    const PI = 3.14
+        const PI = 3.14
 
-    export function areaCircunferencia(raio: number): number {
-        return PI * Math.pow(raio, 2)
-    }
+        export function areaCircunferencia(raio: number): number {
+            return PI * Math.pow(raio, 2)
+        }
 
-    export function areaRetangulo(base: number, altura: number): number {
-        return base * altura
+        export function areaRetangulo(base: number, altura: number): number {
+            return base * altura
+        }
     }
 }
 
 const PI = 2.99
 
-console.log(Areas.areaCircunferencia(10))
-console.log(Areas.areaRetangulo(10, 20))
+console.log(Geometria.Area.areaCircunferencia(10))
+console.log(Geometria.Area.areaRetangulo(10, 20))
 
 console.log(PI)
